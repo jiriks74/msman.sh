64d2a60f8eff6ca3507acaf48129a438ca341f92
Jiří Štefka <jiri.stefka@atlas.cz>
2023-02-27 05:50:46 +0100

    Bugfix version bump

--- msman.sh
+++ msman.sh
@@ -8,7 +8,7 @@ set -e
 #                            and acknowledge the original script and author.                                #
 #############################################################################################################
 
-CURRENT_SCRIPT_VERSION="v2.0.0"
+CURRENT_SCRIPT_VERSION="v2.0.1"
 
 # --------------------------------------------------
 # You shouldn't need to change anything in this file
